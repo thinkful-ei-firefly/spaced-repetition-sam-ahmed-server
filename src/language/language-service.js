d0a77fecca7803424714ad98178945e63e4745be
Sam Walker <jack.nasal@gmail.com>
2019-10-15 17:01:35 -0400

    working on one method of stuff

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -29,14 +29,6 @@ const LanguageService = {
       .where({ language_id })
   },
 
-  getLanguageAndWords(db, language_id) {
-    return db
-      .from('word')
-      .join('language', {'word.language_id': 'language.id'})
-      .select('*')
-      .where({ language_id })
-  },
-
   getLanguageHead(db, id) {
     return db
       .from('language')
@@ -58,13 +50,18 @@ const LanguageService = {
       .first()
   },
   async handleCorrectAnswer(db, language_id, original) {
-    const info = await this.getLanguageAndWords(db, language_id)
-    await db.from('language').where({id:language_id}).update({total_score: info[0].total_score+1})
-    await db.from('word').where({ original }).update({correct_count: info[0].correct_count+1, memory_value: info[0].memory_value*2})
-    await this.moveWordDownList(db, info[0].memory_value*2, info)
+    const language = await this.getLanguage(db, language_id)
+    const score = language.total_score
+    await db.from('language').where({id:1}).update({total_score: score.total_score+1})
+    console.log('ran')
+    const word = await db.from('word').where({ original }).select('correct_count, memory_value').first()
+    await db.from('word').where({ original }).update({correct_count: word.correct_count+1, memory_value: word.memory_value*2})
+    let head = await this.getHead(db, language_id)
+    await this.moveWordDownList(db, word.memory_value*2, language_id)
+    await this.setHead(db, language_id, head.next)
     return this.getLanguageHead(db, language_id)
   },
-  handleIncorrectAnswer(db, language_id, original) {
+  async handleIncorrectAnswer(db, language_id, original) {
     const word = db.from('word').where({ original }).select('incorrect_count').first()
     db.from('word').where({ original }).update({incorrect_count: word.correct_count+1, memory_value: 1})
     let head = this.getHead(db, language_id)
@@ -72,41 +69,36 @@ const LanguageService = {
     this.setHead(db, language_id, head.next)
     return this.getLanguageHead(db, language_id)
   },
-  moveWordDownList(db, n, info) {
-    //nth.next = head
-    //head.next=nth+1
-    //head=head.next
-    headIndex = arr[0].head
-    let i=0
-    while (tempNode.id !==headIndex || i<info.length) {
-      tempNode = arr[i++]
+  async moveWordDownList(db, num, language_id) {
+    const headId = await this.getHead(db, language_id)
+    let head = await this.getNode(db, headId.head)
+    let tempNode = head
+    for (let i=0; i<num;i++) {
+      if (tempNode.next === null) {
+        break
+      } else {
+        tempNode = await this.getNode(db, tempNode.next)
+      }
+      await this.updateNext(head.id, tempNode.next)//head.next = tempNode.next
+      await this.updateNext(tempNode.id, head.id)//tempNode.next = head 
+      await this.updateHead(db, language_id, head.id)//head = tempValue (setHead)
     }
-    head = tempNode
-    let j=0
-    while (tempNode.next !==null) {
-      if(j=n) break
-      tempNode = j++
-    }
-     
-    this.updateNext(head.id, tempNode.next)//head.next = tempNode.next
-    this.updateNext(tempNode.id, head.id)//tempNode.next = head 
-    this.updateHead(db, language_id, head.id)//head = tempValue (setHead)
   },
-  // getNode(db, id) {
-  //   return db
-  //     .from('word')
-  //     .select('id, next')
-  //     .where({ id })
-  //     .first() //{id: #, next: # or null}
-  // },
-  // getHead(db, id) {
-  //   return db
-  //     .from('language')
-  //     .select('head', 'next')
-  //     .where({ id })
-  //     .first() //{head: #}
-  // },
-  updateHead(db, language_id, word_id) {
+  getNode(db, id) {
+    return db
+      .from('word')
+      .select('id, next')
+      .where({ id })
+      .first() //{id: #, next: # or null}
+  },
+  getHead(db, id) {
+    return db
+      .from('language')
+      .select('head', 'next')
+      .where({ id })
+      .first() //{head: #}
+  },
+  setHead(db, language_id, word_id) {
     return db
       .from('language')
       .where({ id: language_id })
@@ -117,10 +109,21 @@ const LanguageService = {
       .from('word')
       .where({ id })
       .update({ next })
+  },
+  getLanguage(db, id) {
+    return db
+      .select('*')
+      .from('language')
+      .where({ id })
+      .first()
+  },
+  getWord(db, id) {
+    return db
+      .select('*')
+      .from('word')
+      .where({ id })
+      .first()
   }
 }
-
-//get all of the info
-//
 
 module.exports = LanguageService
